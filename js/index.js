ce2430e84821468fb98b11bb64ba41aed41fc392
John Paolo Yao <johnpaoloyao94@gmail.com>
2024-04-19 06:41:35 +0800

    update project

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-console.log('loading index.js file...')
+$(window).on('load resize orientationchange', function() {
+  $("#countrySlider").each(function() {
+    var $slider = $(this);
+    if($(window).outerWidth() > 768) {
+      if($slider.hasClass('slick-initialized')) {
+        $slider.slick('unslick');
+      }
+    } else {
+      if (!$slider.hasClass('slick-initialized')) {
+        $slider.slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: false,
+            mobileFirst: true,
+        });
+      }
+    }
+  });
+});
